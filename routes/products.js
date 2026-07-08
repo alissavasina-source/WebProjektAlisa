8eaed0b7f66d2ec182fe92061e14ccb61fe82827
Brenda Nzweffo Lontsi <brenda.nzweffo-lontsi@informatik.hs-fulda.de>
2026-07-08 11:27:14 +0200

    products in view anpassen

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,37 @@ const { requireLogin, requireTeam } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', controller.list);
+router.post('/', requireTeam, controller.create);
+router.post('/:id/bearbeiten', requireTeam, controller.update);
+router.post('/:id/loeschen', requireTeam, controller.remove);
 router.post('/:id/warenkorb', requireLogin, controller.addToCart);
 router.get('/:id', controller.details);
 
+/*für plus */
+router.post("/:id/plus", (req, res) => {
+const cart = req.session.cart || [];
+    const item = cart.find(
+        p => p.productId === req.params.id
+    );
+
+    if(item){
+        item.quantity++;
+    }
+
+    res.redirect("/warenkorb");
+});
+
+/*für minus */
+router.post("/:id/minus", (req, res) => {
+const cart = req.session.cart || [];
+    const item = cart.find(
+        p => p.productId === req.params.id
+    );
+
+    if(item && item.quantity > 1){
+        item.quantity--;
+    }
+
+    res.redirect("/warenkorb");
+});
 module.exports = router;
