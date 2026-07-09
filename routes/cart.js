c2c0c5641d86ed74c823e3282dd4ef0a0f65a2d5
Brenda Nzweffo Lontsi <brenda.nzweffo-lontsi@informatik.hs-fulda.de>
2026-07-09 15:50:43 +0200

    Menge Steuerungen anpassen

--- routes/cart.js
+++ routes/cart.js
@@ -8,4 +8,26 @@ router.get('/', requireLogin, controller.showCart);
 router.post('/checkout', requireLogin, controller.checkout);
 router.post('/:id/entfernen', requireLogin, controller.removeFromCart);
 
+router.post("/:id/plus", (req, res) => {
+  const cart = req.session.cart || [];
+  const item = cart.find(p => p.productId === req.params.id);
+
+  if (item) {
+    item.quantity++;
+  }
+
+  res.redirect("/warenkorb");
+});
+
+router.post("/:id/minus", (req, res) => {
+  const cart = req.session.cart || [];
+  const item = cart.find(p => p.productId === req.params.id);
+
+  if (item && item.quantity > 1) {
+    item.quantity--;
+  }
+
+  res.redirect("/warenkorb");
+});
+
 module.exports = router;
