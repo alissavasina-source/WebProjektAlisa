2b0e01a6f49b03ed6554754fb80dc84da3089952
fd0005496 <alisa.vasina@users.noreply.git-ce.aachen.de>
2026-07-13 13:53:53 +0200

    einfugen von anderen Funktionen

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  salonName: 'Haarschnitt-Atelie-Kassel',
+  salonName: 'H-Haarschnitt-Atelier-Kassel',
   port: process.env.PORT || 3000,
   sessionSecret: process.env.SESSION_SECRET || 'friseursalon-geheim',
   dataDir: './data',
